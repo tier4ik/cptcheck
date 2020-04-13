cc46e9e74eddcdc8a5cd7a81a08d211aeee4e63e
Max Shevchencko <tier4ik@gmail.com>
2020-04-13 16:20:16 +0400

    keyController uses prompt to set API key. Prompt is generated by 'inquirer' package. Also added 'colors' package.

--- controllers/KeyController.js
+++ controllers/KeyController.js
@@ -1,11 +1,38 @@
+const Configstore = require('configstore');
+const inquirer = require('inquirer');
+const colors = require('colors');
+
+const packageJson = require('../package.json');
+
+const store = new Configstore(packageJson.name);
+
 module.exports = {
-    set() {
-        console.log('new key has been set')
+    async set() {
+        const input = await inquirer.prompt([
+            {
+                type: 'input',
+                name: 'key',
+                message: 'Enter API key'.green
+            }
+        ])
+        store.set('key', input.key)
+        console.log('New API key has been set'.blue);
+        return input.key;
     },
     show() {
-        console.log('key')
+        const key = store.get('key');
+        if (!key) { 
+            console.log('You doesn`t have API key for https://nomics.com')
+        } else {
+            console.log('API key for https://nomics.com - ' + key);
+            return key;
+        }
     },
     remove() {
-        console.log('key has been removed')
+        const keyToDelete = store.get('key');
+        if (!keyToDelete) throw new Error('You doesn`t have API key for https://nomics.com');
+        store.delete('key');
+        console.log('API key ' + keyToDelete + ' has been removed');
+        return keyToDelete;
     }
 }
